00ecefcf58e988ea2423a80e52d97f8ea48f366f
kturnal <batuker.kagan@windowslive.com>
2025-02-25 12:58:52 +0300

    proper initialization & add Raffle.sol

---
+++ src/Raffle.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title A sample Raffle Contract
+ * @author Patrick Collins (or even better, you own name)
+ * @notice This contract is for creating a sample raffle
+ * @dev It implements Chainlink VRFv2.5 and Chainlink Automation
+ */
+contract Raffle {
+
+    function enterRaffle() public {}
+
+    function pickWinner() public {}
+}
